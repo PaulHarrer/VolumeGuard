47eb1c0f9c8f73dad0b12d38a800b1a8f7b05d30
PaulHarrer <mail@paulharrer.at>
2023-03-26 16:44:46 +0200

    Patch: updated main functionality

--- popup.js
+++ popup.js
@@ -1,3 +1,41 @@
+// Update the volume and badge whenever the tab is switched or reloaded
+function updateVolumeAndBadge(tab) {
+  const tabId = tab.id;
+
+  chrome.tabs.executeScript(tabId, {
+    code: `Array.from(document.querySelectorAll('video, audio')).map(media => media.volume)[0];`
+  }, (result) => {
+    const volume = Math.round(result[0] * 100);
+
+    // Update the volume slider
+    document.getElementById('volume').value = volume;
+
+    // Update the extension badge
+    chrome.browserAction.setBadgeText({ text: volume.toString(), tabId: tabId });
+  });
+}
+
+// Initialize the volume and badge on extension load
+chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+  const activeTab = tabs[0];
+  updateVolumeAndBadge(activeTab);
+});
+
+// Update the volume and badge whenever the tab is switched or reloaded
+chrome.tabs.onActivated.addListener((activeInfo) => {
+  const tabId = activeInfo.tabId;
+  chrome.tabs.get(tabId, (tab) => {
+    updateVolumeAndBadge(tab);
+  });
+});
+
+chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
+  if (changeInfo.status === 'complete' && tab.active) {
+    updateVolumeAndBadge(tab);
+  }
+});
+
+// Update the volume whenever the slider changes
 document.getElementById('volume').addEventListener('change', (event) => {
   const volume = event.target.value / 100;
 
@@ -8,6 +46,10 @@ document.getElementById('volume').addEventListener('change', (event) => {
     chrome.tabs.executeScript(tabId, {
       code: `Array.from(document.querySelectorAll('video, audio')).forEach(media => media.volume = ${volume});`
     });
+
+    // Update the badge with the new volume
+    const badgeText = Math.round(volume * 100).toString();
+    chrome.browserAction.setBadgeText({ text: badgeText, tabId: tabId });
   });
 });
 
@@ -19,5 +61,6 @@ document.getElementById('reset').addEventListener('click', () => {
     chrome.tabs.executeScript(tabId, {
       code: `Array.from(document.querySelectorAll('video, audio')).forEach(media => media.volume = 1);`
     });
+    updateVolumeAndBadge(activeTab);
   });
 });
